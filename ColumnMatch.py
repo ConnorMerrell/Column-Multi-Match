ef692b414c9a07d7570784b787b57989593b0981
ConnorMerrell <138818788+ConnorMerrell@users.noreply.github.com>
2023-07-11 22:14:21 +0100

    Update ColumnMatch.py
    added dataframe export

--- ColumnMatch.py
+++ ColumnMatch.py
@@ -11,8 +11,18 @@ df = pd.read_csv("mergedpostcodes.csv")
 y_values = set(df[y].tolist())
 y_values = list(y_values)
 
+finallist = []
 
 for i in (y_values):
     df2 = df[df[y].isin([i])]
-    print(i + " = " + str(df2[x].tolist()))
+    #print(i + " = " + str(df2[x].tolist()))
+    finallist.append([i,str(df2[x].tolist())])
+
+finaldf = pd.DataFrame(finallist)
+
+finaldf.to_csv("list.csv", index=False, header=None)
+print("done")
+
+
+
 
